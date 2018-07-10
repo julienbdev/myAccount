ace47a6fbfbe7a80b51065dd2db53f4342ebd0dc
julien <julien1982@hotmail.com>
2018-07-10 16:50:19 +0200

    Init springboot project

---
+++ src/main/java/com/julienbdev/mymoney/MymoneyApplication.java
@@ -0,0 +1,12 @@
+package com.julienbdev.mymoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymoneyApplication.class, args);
+	}
+}
